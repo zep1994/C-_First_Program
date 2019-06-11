bb387fab46a04ce11a2f2b83b70ffd66c3c9dde4
Thomas Matlock <32077179+zep1994@users.noreply.github.com>
2019-06-11 17:33:49 -0500

    Use CIN and set to return string

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 
 int main() {
-       int favorite_number;
-       std::cout << "Enter your favorite number between 1 and 100: ";
-       
-       std::cin >> favorite_number;
-       
-       std::cout << "Amazing that is my favorite number too" << std::endl;
-       std::cout << "No really, " << favorite_number << " is my favorite number";
+//       int favorite_number;
+//       std::cout << "Enter your favorite number between 1 and 100: ";
+//       
+//       std::cin >> favorite_number;
+//       
+//       std::cout << "Amazing that is my favorite number too" << std::endl;
+//       std::cout << "No really, " << favorite_number << " is my favorite number";
+
+        char name[100]; 
+        std::cout << "Enter your name: " << std::endl;
+        std::cin >> name;
+        std::cout << "Your name sucks " << name << std::endl;
        return 0;
  }
