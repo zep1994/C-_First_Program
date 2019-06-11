a37c84503155bd81266835efb1326330ebda307f
Thomas Matlock <32077179+zep1994@users.noreply.github.com>
2019-06-11 18:08:19 -0500

    Work on var declaration

--- main.cpp
+++ main.cpp
@@ -9,9 +9,26 @@ int main() {
 //       std::cout << "Amazing that is my favorite number too" << std::endl;
 //       std::cout << "No really, " << favorite_number << " is my favorite number";
 
-        char name[100]; 
-        std::cout << "Enter your name: " << std::endl;
-        std::cin >> name;
-        std::cout << "Your name sucks " << name << std::endl;
-       return 0;
+//        char name[100]; 
+//        std::cout << "Enter your name: " << std::endl;
+//        std::cin >> name;
+//        std::cout << "Your name sucks " << name << std::endl;
+//       return 0;
+
+    int width {0};
+    int height {0}; 
+    int area {0};
+    
+    std::cout << "Enter width and height of the room separated by a space " << std::endl;
+    std::cin >> width >> height;
+    area = width * height;
+    std::cout << area << std::endl;
+
+
+
+
+
+
+
+
  }
