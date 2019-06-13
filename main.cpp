8399767e7feface34d49e0b26c10251f7180b224
Thomas Matlock <32077179+zep1994@users.noreply.github.com>
2019-06-13 15:18:18 -0500

    Hotel Problem

--- main.cpp
+++ main.cpp
@@ -15,20 +15,37 @@ int main() {
 //        std::cout << "Your name sucks " << name << std::endl;
 //       return 0;
 
-    int width {0};
-    int height {0}; 
-    int area {0};
+//    int width {0};
+//    int height {0}; 
+//    int area {0};
+//    
+//    std::cout << "Enter width and height of the room separated by a space " << std::endl;
+//    
+//    std::cin >> width >> height;
+//    
+//    area = width * height;
+//    
+//    std::cout << area << std::endl;
+
+
+    //HOTEL PROBLEM
     
-    std::cout << "Enter width and height of the room separated by a space " << std::endl;
-    std::cin >> width >> height;
-    area = width * height;
-    std::cout << area << std::endl;
-
-
-
-
-
-
+    
+    std::cout << "How many rooms?:\n";
+    
+    int number_of_rooms {0};
+    std::cin >> number_of_rooms;
+    
+    const double price_per_room {30};
+    const double tax {0.07};
+    
+    std::cout << number_of_rooms << "\n";
+    
+    std::cout << "Cost per room is " << price_per_room * number_of_rooms << std::endl;
+    std::cout << "with tax " << price_per_room * number_of_rooms * tax << std::endl;
+    
+    std::cout << "Total estimate " << (price_per_room * number_of_rooms) + (price_per_room * number_of_rooms * tax) << std::endl;
+    
 
 
  }
